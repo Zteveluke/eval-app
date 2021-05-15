ce7fbc6ff0a7fdfa2835b2d59ca03a345710273f
Zteveluke <52157482+Zteveluke@users.noreply.github.com>
2021-05-16 00:09:26 +0530

    m

---
+++ db_connect.php
@@ -0,0 +1,5 @@
+<?php 
+
+//$conn= new mysqli('localhost','root','','evaluation_db')or die("Could not connect to mysql".mysqli_error($con));
+
+$conn= new mysqli('remotemysql.com','XdvXJGPR7G','e5n34GWRUh','XdvXJGPR7G')or die("Could not connect to mysql".mysqli_error($con));
