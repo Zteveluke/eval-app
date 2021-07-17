f9c7ab6ab8555e543709031246ef08c19b912076
Steve Luke <52157482+Zteveluke@users.noreply.github.com>
2021-07-17 22:09:26 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,4 +2,4 @@
 
 //$conn= new mysqli('localhost','root','','evaluation_db')or die("Could not connect to mysql".mysqli_error($con));
 
-$conn= new mysqli('remotemysql.com','XdvXJGPR7G','e5n34GWRUh','XdvXJGPR7G')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('sql6.freemysqlhosting.net','sql6426044','SzUNXa3i2K','sql6426044')or die("Could not connect to mysql".mysqli_error($con));
